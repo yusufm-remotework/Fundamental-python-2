8c26ea59ab5241de0bba4e66e425ac9e81f7a33b
YUSUF-NOTEBOOK <yusufm2391@gmail.com>
2022-02-19 22:44:24 +0700

    initial commit python fundamental

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+semua sintaksis dasar bahasa pemograman terdiri dari:
+1. Sekuensial: langkah berurutan
+2. percabangan: langkah melompat jika kondisi terpenuhi
+3. perulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
+
+# Percabangan
+jumlah_botol_susu = 0
+if jumlah_botol_susu >0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    print("membeli 1 botol 1")
+else:
+    print("tidak jadi beli")
+
+print("Budi pulang kerumah")
+print("menyampaikan hasilnya kepada Ibu")
