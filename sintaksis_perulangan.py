ce96fc03e1a9ecc90b1d97495489f1ac8743e681
YUSUF-NOTEBOOK <yusufm2391@gmail.com>
2022-02-20 22:36:42 +0700

    Sintaksis perulangan

---
+++ sintaksis_perulangan.py
@@ -0,0 +1,12 @@
+"""
+Program perulangan membaca buku
+"""
+jumlah_buku = 10
+print('"Ibu Berkata", baca semua bukumu!')
+
+jumlah_buku_yang_sudah_dibaca = 0
+print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
+
+for jumlah_buku_yang_sudah_dibaca in range(1,jumlah_buku+1):
+    print(f"Buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
