a10135341aa50e4141cc321bd7d4ba8e01aa29e2
YUSUF-NOTEBOOK <yusufm2391@gmail.com>
2022-02-25 08:00:29 +0700

    type data list

---
+++ Sintaksis-type_data_list.py
@@ -0,0 +1,57 @@
+daftar_buku = ['Seven habits','How to influence people','First thing first','4dx']
+print("\ntampilkan semua daftar buku")
+print(daftar_buku)
+
+print("\ntampilkan semua dengan for in")
+for buku in daftar_buku:
+    print(buku)
+
+print("\ntampilkan isi daftar_buku dengan index tertentu")
+print(daftar_buku[0])
+
+print("\ntampilkan dengan in range")
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+daftar_buku = [1, "kenji volume 2", -313, 3.14, "accounting"]
+print("\ntampilkan dengan in range denga tipe data tiap elemen berbeda2")
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print("\nkembalikan nilai awal daftar buku")
+daftar_buku = ['Seven habits','How to influence people','First thing first','4dx']
+print("\ntambahkan 1 buku")
+daftar_buku.append('Dunia matematika dunia kelas 5')
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print("\nClear list")
+daftar_buku.clear()
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print("\nGanti element pertama")
+daftar_buku =  ['Seven habits','How to influence people','First thing first','4dx']
+daftar_buku[0]= 'Eight Habit'
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nAmbil elemen ke-2')
+buku = daftar_buku.pop(1)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print("\nBuku yang diambil tadi")
+print(buku)
+
+print('n\pop')
+daftar_buku.pop()
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku )
+
+print('n\pop -1')
+
+daftar_buku = ['Seven habits','How to influence people','First thing first','4dx']
+daftar_buku.pop(-4)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
--- sintaksis_perulangan_while_Unfined_count.py
+++ sintaksis_perulangan_while_Unfined_count.py
@@ -1,22 +1,20 @@
 """
 Program perulangan membaca buku dengan while sampai paham
 """
-Jumlah_buku = 10
+jumlah_buku = 10
 
-print ('Ibu berkata, "Baca semua bukumu!!"')
+print ('Ibu berkata, "Baca Semua buku dan pahami"')
 
-total_jumlah_baca =0
+total_jumlah_baca = 0
+jumlah_paham =0
+print (f"total jumlah baca dan paham {total_jumlah_baca,jumlah_paham}")
 
-Jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
-print(f"Jumlah buku yang dibaca dan dipahami {Jumlah_buku_yang_sudah_dibaca_dan_dipahami}")
-
-while total_jumlah_baca < Jumlah_buku *2:
+while total_jumlah_baca < jumlah_buku:
     total_jumlah_baca = total_jumlah_baca +1
-    if Jumlah_buku_yang_sudah_dibaca_dan_dipahami == 9:
-        print(f"buku ke {Jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1} belum paham")
+    if jumlah_paham== 5:
+        print(f"buku ke {jumlah_paham+1} belum paham")
     else:
-        Jumlah_buku_yang_sudah_dibaca_dan_dipahami = Jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
-        print (f"Buku ke {Jumlah_buku_yang_sudah_dibaca_dan_dipahami} sudah dibaca dan dipahami")
+        jumlah_paham = jumlah_paham +1
+        print(f"buku ke{jumlah_paham} sudah paham")
 
 
-print(f"Jumlah buku yang sudah dibaca dan dipahami {Jumlah_buku_yang_sudah_dibaca_dan_dipahami}")
